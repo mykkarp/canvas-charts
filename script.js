f9587f86688d895eed61fedd55a49512e3ef89d7
mykkarp <mykkarp@gmail.com>
2022-01-18 19:59:29 +0300

    feat: additional data

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+import { getChartData } from './data.js';
+
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 200;
 const PADDING = 40;
 const DPI_CANVAS_WIDTH = CANVAS_WIDTH * 2;
 const DPI_CANVAS_HEIGHT = CANVAS_HEIGHT * 2;
 const CANVAS_VIEW_HEIGHT = DPI_CANVAS_HEIGHT - PADDING * 2;
+const CANVAS_VIEW_WIDTH = DPI_CANVAS_WIDTH;
 const ROWS_COUNT = 5;
 
 function chart(canvas, data) {
@@ -15,7 +18,8 @@ function chart(canvas, data) {
 
   const [yMin, yMax] = computeBoundaries(data);
   const textStep = (yMax - yMin) / ROWS_COUNT;
-  const yRation = CANVAS_VIEW_HEIGHT / (yMax - yMin);
+  const yRatio = CANVAS_VIEW_HEIGHT / (yMax - yMin);
+  const xRatio = CANVAS_VIEW_WIDTH / (data.columns[0].length - 2);
 
   // === y axis
   const step = CANVAS_VIEW_HEIGHT / ROWS_COUNT;
@@ -26,7 +30,7 @@ function chart(canvas, data) {
   ctx.fillStyle = '#96a2aa';
   for (let i = 1; i <= ROWS_COUNT; i++) {
     const y = i * step + PADDING;
-    const text = yMax - textStep * i;
+    const text = Math.round(yMax - textStep * i);
     ctx.fillText(text.toString(), 5, y - 10);
     ctx.moveTo(0, y);
     ctx.lineTo(DPI_CANVAS_WIDTH, y);
@@ -35,34 +39,56 @@ function chart(canvas, data) {
   ctx.closePath();
   // ===
 
+  data.columns.forEach(col => {
+    const name = col[0];
+    if (data.types[name] === 'line') {
+      const coords = col.map((y, i) => [
+        Math.floor((i - 1) * xRatio),
+        Math.floor(DPI_CANVAS_HEIGHT - PADDING - y * yRatio)
+      ])
+      coords.shift();
+
+      const color = data.colors[name];
+      drawLines(ctx, coords, { color });
+    }
+  });
+}
+
+chart(document.querySelector('#chart'), getChartData());
+
+function drawLines(ctx, coords, { color }) {
   ctx.beginPath();
-  for (const [x, y] of data) {
-    ctx.lineTo(x, DPI_CANVAS_HEIGHT - PADDING - y * yRation);
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = '#ff0000';
-    ctx.stroke();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = color;
+  for (const [x, y] of coords) {
+    ctx.lineTo(x, y);
   }
+  ctx.stroke();
   ctx.closePath();
 }
 
-chart(document.querySelector('#chart'), [
-  [0, 0],
-  [50, 120],
-  [120, 200],
-  [200, 300],
-  [300, 220]
-]);
-
-function computeBoundaries(data) {
+function computeBoundaries({ columns, types }) {
   let min = null;
   let max = null;
 
-  for (const [, y] of data) {
-    if (typeof min !== 'number') min = y;
-    if (typeof max !== 'number') max = y;
-    if (min > y) min = y;
-    if (max < y) max = y;
-  }
+  console.log(types);
+  columns.forEach(column => {
+    if (types[column[0]] !== 'line') {
+      return;
+    }
+
+    if (typeof min !== 'number') min = column[1];
+    if (typeof max !== 'number') max = column[1];
+
+    if (min > column[1]) min = column[1];
+    if (max < column[1]) max = column[1];
+
+    for (let i = 2; i < column.length; i++) {
+      if (min > column[i]) min = column[i];
+      if (max < column[i]) max = column[i];
+    }
+  });
+
 
   return [min, max];
 }
